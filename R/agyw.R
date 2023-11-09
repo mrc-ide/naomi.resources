8a01daf1fc5b809c631f3f47ccdee071bfd63cbc
Rob Ashton <r.ashton@imperial.ac.uk>
2023-11-09 17:07:47 +0000

    Add function to return info about country resources

--- R/agyw.R
+++ R/agyw.R
@@ -11,25 +11,29 @@
 load_agyw_exdata <- function(data, iso3) {
   assert_scalar_character(data)
   assert_scalar_character(iso3)
-  agyw_data_root <- system_file(file.path("extdata", "agyw"))
-  validate_inputs(agyw_data_root, iso3)
-  iso3 <- toupper(iso3)
+  country_data <- get_country_data_path(
+    system_file(file.path("extdata", "agyw")),
+    iso3)
+
+  if (data == "zaf_propensity" && toupper(iso3) != "ZAF") {
+    cli::cli_abort("Can't get zaf_propensity data for country '{iso3}'.")
+  }
 
   path <- switch(
     data,
-    "srb_female" = file.path(iso3, "female_best-3p1-multi-sexbehav-sae.csv"),
-    "srb_male" = file.path(iso3, "male_best-3p1-multi-sexbehav-sae.csv"),
-    "srb_survey_female" = file.path(iso3, "male_best-3p1-multi-sexbehav-sae.csv"),
-    "srb_survey_male" = file.path(iso3, "male_hiv_indicators_sexbehav.csv"),
-    "fsw_pse" = file.path(iso3, "fsw_pse.csv"),
-    "msm_pse" = file.path(iso3, "msm_pse.csv"),
-    "pwid_pse" = file.path(iso3, "pwid_pse.csv"),
-    "afs" = file.path(iso3, "kinh_afs_dist.csv"),
-    "zaf_propensity" = file.path("ZAF", "zaf_propensity.csv"),
+    "srb_female" = file.path("female_best-3p1-multi-sexbehav-sae.csv"),
+    "srb_male" = file.path("male_best-3p1-multi-sexbehav-sae.csv"),
+    "srb_survey_female" = file.path("male_best-3p1-multi-sexbehav-sae.csv"),
+    "srb_survey_male" = file.path("male_hiv_indicators_sexbehav.csv"),
+    "fsw_pse" = file.path("fsw_pse.csv"),
+    "msm_pse" = file.path("msm_pse.csv"),
+    "pwid_pse" = file.path("pwid_pse.csv"),
+    "afs" = file.path("kinh_afs_dist.csv"),
+    "zaf_propensity" = file.path("zaf_propensity.csv"),
     cli::cli_abort("Can't locate data of type '{data}'.")
   )
 
-  path <- file.path(agyw_data_root, path)
+  path <- file.path(country_data, path)
   read_naomi_resource(path)
 }
 
@@ -43,19 +47,19 @@ load_agyw_exdata <- function(data, iso3) {
 get_agyw_workbook_path <- function(iso3) {
   assert_scalar_character(iso3)
   agyw_data_root <- system_file(file.path("extdata", "agyw"))
-  validate_inputs(agyw_data_root, iso3)
-  iso3 <- toupper(iso3)
+  country_data <- get_country_data_path(agyw_data_root, iso3)
 
-  file.path(agyw_data_root, iso3, "pse_workbook_template.xlsx")
+  file.path(country_data, "pse_workbook_template.xlsx")
 }
 
-validate_inputs <- function(agy_data_path, iso3) {
+get_country_data_path <- function(agy_data_path, iso3) {
   available_iso3 <- list.files(agy_data_path)
   if (!(tolower(iso3) %in% tolower(available_iso3))) {
     available <- paste(available_iso3, collapse = "', '")
     cli::cli_abort(c("Can't locate data for iso3 '{iso3}'",
                      i = "Available countries are '{available}'."))
   }
+  invisible(file.path(agy_data_path, toupper(iso3)))
 }
 
 read_naomi_resource <- function(path) {
---
+++ R/package_info.R
@@ -0,0 +1,31 @@
+#' Get info about resources for a country
+#'
+#' This returns a list containing
+#'   * The version of the package installed
+#'   * The URL from the DESCRIPTION
+#'   * A data frame with the files and their hashes for this country
+#'
+#' @param iso3 Country iso3 to get resources info for
+#'
+#' @return A list containing info about resources for this country
+#' @export
+get_resources_info <- function(iso3) {
+  assert_scalar_character(iso3)
+  root <- system_file()
+  country_data <- get_country_data_path(file.path(root, "extdata", "agyw"),
+                                        iso3)
+
+  files <- list.files(country_data, full.names = TRUE)
+  country_relative_path <- sub(paste0(root, "/"), "", country_data)
+  relative_paths <- file.path(country_relative_path, basename(files))
+  hash <- unname(tools::md5sum(files))
+
+  resources_version <- utils::packageVersion("naomi.resources")
+  resources_url <- utils::packageDescription("naomi.resources")$URL
+
+  list(
+    version = resources_version,
+    url = resources_url,
+    files = data.frame(filename = relative_paths, hash = hash)
+  )
+}
