f5f8716dd745db048382446873184447a3325027
Rob Ashton <r.ashton@imperial.ac.uk>
2023-11-10 09:19:17 +0000

    Fix typo in test

--- tests/testthat/test-package-info.R
+++ tests/testthat/test-package-info.R
@@ -1,6 +1,6 @@
 test_that("can get info about a countries data", {
   info <- get_resources_info("BWA")
-  expect_equal(info$version, tools::packageVerision("naomi.resources"))
+  expect_equal(info$version, tools::packageVersion("naomi.resources"))
   expect_true(grepl("https://github.com/mrc-ide/", info$url, fixed = TRUE))
   expect_equal(colnames(info$files), c("filename", "hash"))
   expect_true(all(grepl("extdata/agyw/BWA/", info$files$filename,
